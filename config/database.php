d551b478f4bbc2fcb316c0320c2c4cb450f18d44
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2018-02-03 16:32:21 +0000

    add aws rds database

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'nell.ctewbdzoj1q8.eu-west-2.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'myelements'),
+            'username' => env('DB_USERNAME', 'drsaulwiggin'),
+            'password' => env('DB_PASSWORD', 'Mbcx5sw40!'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
