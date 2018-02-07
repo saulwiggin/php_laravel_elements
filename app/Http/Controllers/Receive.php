dc89630fce6b4e13d668e811b79a08dc7fd939e3
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2018-02-07 11:09:50 +0000

    retreive data from 23andme demo_oauth_user

--- app/Http/Controllers/Receive.php
+++ app/Http/Controllers/Receive.php
@@ -8,10 +8,30 @@ class Receive extends Controller
 {
   public function index()
   {
-      $request = $_GET['code'];
-      var_dump($request);
+      $dna_token = $_GET['code'];
 
-      
+      $client = new \GuzzleHttp\Client();
+      $result = $client->get('https://api.23andme.com/3/account/', [
+        'headers' => [
+            'Authorization' => 'Bearer demo_oauth_token'
+          ],
+        'form_params' => [
+          'client_id' => 'a2e401e2dc7aa172df1b466597edcb8b',
+          'client_secret' => '3f7fd6d92dc8286b5704103870419be1',
+          'grant_type' => 'authorization_code',
+          'code' => $dna_token,
+          'redirect_uri' => 'http://localhost:8000/receive/',
+          'scope' => 'basic names email'
+        ]
+      ]);
+
+    $contents = $result->getBody()->getContents();
+    $contents = json_decode($contents);
+
+    var_dump($contents);
+
+    //return to DatabaseServiceProvider
+
       //return view('marketing');
   }
 }
