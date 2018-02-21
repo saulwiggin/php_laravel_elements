5e923ed6dbcee54b2e0c85aa78636e6b5e2603cf
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2018-02-21 14:18:04 +0000

    ignore social for now

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -67,7 +67,7 @@
                                 </button>
 
                                 <!-- facebook login -->
-                            <div class="fb-login-button" data-width="77" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>                            </div>
+                            <!-- <div class="fb-login-button" data-width="77" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>                            </div> -->
                         </div>
                     </form>
                 </div>
