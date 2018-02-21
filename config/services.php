676a95405b5fef58dced8a37b84aa1d80a16d8cd
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2018-02-21 16:36:04 +0000

    add twitter google linkedin via socialite

--- config/services.php
+++ config/services.php
@@ -35,4 +35,28 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'facebook' => [
+      'client_id' => env('GITHUB_CLIENT_ID'),         // Your GitHub Client ID
+      'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
+      'redirect' => 'http://your-callback-url',
+    ],
+
+    'twitter' => [
+      'client_id' => env('GITHUB_CLIENT_ID'),         // Your GitHub Client ID
+      'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
+      'redirect' => 'http://your-callback-url',
+    ],
+
+    'google' => [
+      'client_id' => env('GITHUB_CLIENT_ID'),         // Your GitHub Client ID
+      'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
+      'redirect' => 'http://your-callback-url',
+    ],
+
+    'linkedin' => [
+      'client_id' => env('GITHUB_CLIENT_ID'),         // Your GitHub Client ID
+      'client_secret' => env('GITHUB_CLIENT_SECRET'), // Your GitHub Client Secret
+      'redirect' => 'http://your-callback-url',
+    ],
+
 ];
