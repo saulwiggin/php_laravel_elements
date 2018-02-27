f0ee958410deadbc12b4624473bbae88c962cd68
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2018-02-27 11:58:40 +0000

    add variant model

--- app/Http/Controllers/brain.php
+++ app/Http/Controllers/brain.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\snpmatrix;
+use App\variants;
+
 
 class brain extends Controller
 {
@@ -14,14 +17,26 @@ class brain extends Controller
   public function upload23andme()
   {
     // upload last 23andme data
+    //profile id /last uploaded
+
+    //first gene rs12794714 vitamin D
+
+    //$marker_data = marker
+    $variant_data = variants
 
     // match SNP's
+    $master_snp_matrix = snpmatrix::::where('In23andme', 1)
+               ->get();
 
-    // display data
+    // report results after matching send to tabel for that users
+
+    
   }
-  public function txtDNAupload()
+  public function txtDNAupload($firstname, $lastname)
   {
     //  upload last uploaded genome
+    $filename = $firstname . $lastname . '.txt';
+      $contents = Storage::get($filename);
 
     // SNP matrix
 
